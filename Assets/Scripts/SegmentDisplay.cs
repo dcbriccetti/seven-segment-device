97a308afd60882f1bd3c446abe4d88e6df649cb0
Dave Briccetti <daveb@davebsoft.com>
2020-08-27 14:46:50 -0700

    Improve code quality

--- Assets/Scripts/SegmentDisplay.cs
+++ Assets/Scripts/SegmentDisplay.cs
@@ -1,27 +1,39 @@
-﻿using System;
-using UnityEngine;
+﻿using UnityEngine;
 using static System.Linq.Enumerable;
+using static SegmentDisplay.Orientation;
 
 public class SegmentDisplay : MonoBehaviour {
     public Transform segmentPrefab;
     public Material onMaterial;
     public Material offMaterial;
     private Transform[] segments;
+    private const int NumSegments = 7;
 
-    private static int Encode(params int[] segments) => 
-        segments.Aggregate(0, (sum, x) => sum + (int) Math.Pow(2, x));
+    private class Shape {
+        private readonly byte shapeBits;
 
-    private static readonly int[] litSegments0To9 = {
-        Encode(0,    2, 3, 4, 5, 6),
-        Encode(            4,    6),
-        Encode(0, 1, 2,    4, 5   ),    //   |    2    | 
-        Encode(0, 1, 2,    4,    6),    //   |3  ---  4| 
-        Encode(   1,    3, 4,    6),    //   |    1    | 
-        Encode(0, 1, 2, 3,       6),    //   |5  ---  6| 
-        Encode(0, 1, 2, 3,    5, 6),    //   |    0    | 
-        Encode(      2,    4,    6),    //       ---     
-        Encode(0, 1, 2, 3, 4, 5, 6),
-        Encode(   1, 2, 3, 4, 6   )
+        public Shape(params byte[] segmentIndexes) {
+            shapeBits = (byte) segmentIndexes.Aggregate(0, (segmentBits, segmentIndex) => 
+                segmentBits | (1 << segmentIndex));
+        }
+
+        public bool HasSegment(int segmentIndex) {
+            int bit = 1 << segmentIndex;
+            return (shapeBits & bit) == bit;
+        }
+    }
+
+    private static readonly Shape[] digitShapes = {
+        new Shape(0,    2, 3, 4, 5, 6),
+        new Shape(            4,    6),
+        new Shape(0, 1, 2,    4, 5   ),    //   |    2    | 
+        new Shape(0, 1, 2,    4,    6),    //   |3  ---  4| 
+        new Shape(   1,    3, 4,    6),    //   |    1    | 
+        new Shape(0, 1, 2, 3,       6),    //   |5  ---  6| 
+        new Shape(0, 1, 2, 3,    5, 6),    //   |    0    | 
+        new Shape(      2,    4,    6),    //       ---     
+        new Shape(0, 1, 2, 3, 4, 5, 6),
+        new Shape(   1, 2, 3, 4, 6   )
     };
 
     private int digitShowing;
@@ -31,28 +43,45 @@ public class SegmentDisplay : MonoBehaviour {
         LightSegments();
     }
 
-    private void Awake() {
-        Bounds segmentBounds = GetComponent<MeshFilter>().sharedMesh.bounds;
+    public enum Orientation { Horizontal, Vertical };
+    
+    private class Geom {
+        public readonly float xFraction;
+        public readonly float yFraction;
+        public readonly Orientation orientation;
+        public Geom(int xPercent, int yPercent, Orientation orientation) {
+            this.orientation = orientation;
+            xFraction = xPercent / 100f;
+            yFraction = yPercent / 100f;
+            this.orientation = orientation;
+        }
+    }
+
+    private static readonly Geom[] segmentGeometries = {
+        new Geom( 50,   0, Horizontal),
+        new Geom( 50,  50, Horizontal),
+        new Geom( 50, 100, Horizontal),
+        new Geom(  0,  75, Vertical  ),
+        new Geom(100,  75, Vertical  ),
+        new Geom(  0,  25, Vertical  ),
+        new Geom(100,  25, Vertical  )
+    };
+
+    private void Start() {
+        var segmentBounds = GetComponent<MeshFilter>().sharedMesh.bounds;
         segmentBounds.Expand(-segmentBounds.size.y * .27f); // Leave a margin
-        float[] x =   {   .5f,   .5f,   .5f,    0,    1,    0,    1 };
-        float[] y =   {     0,   .5f,     1, .75f, .75f, .25f, .25f };
-        bool[] vert = { false, false, false, true, true, true, true };
+        Bounds bounds = segmentBounds;
         var pos = transform.position;
-        Vector3 dims = segmentBounds.size;
-        var min = segmentBounds.min;
-        segments = Range(0, 7).Select(i => Instantiate(segmentPrefab, new Vector3(
-            pos.x + min.x + x[i] * dims.x,
-            pos.y + min.y + y[i] * dims.y, 
-            -.1f), Quaternion.Euler(0, 0, vert[i] ? 0 : 90), transform)).ToArray();
+        segments = segmentGeometries.Select(segGeom => Instantiate(segmentPrefab, new Vector3(
+            pos.x + bounds.min.x + segGeom.xFraction * bounds.size.x,
+            pos.y + bounds.min.y + segGeom.yFraction * bounds.size.y,
+            -.1f), Quaternion.Euler(0, 0, segGeom.orientation == Vertical ? 0 : 90), transform)).ToArray();
     }
 
     private void LightSegments() {
-        var segmentBits = litSegments0To9[digitShowing];
-        for (int i = 0; i < 7; i++) {
-            var seg = segments[i].gameObject;
-            var segmentBit = (int) Math.Pow(2, i);
-            var segOn = (segmentBit & segmentBits) != 0;
-            seg.GetComponent<Renderer>().material = segOn ? onMaterial : offMaterial;
+        for (int i = 0; i < NumSegments; i++) {
+            var segmentRenderer = segments[i].gameObject.GetComponent<Renderer>();
+            segmentRenderer.material = digitShapes[digitShowing].HasSegment(i) ? onMaterial : offMaterial;
         }
     }
 }
